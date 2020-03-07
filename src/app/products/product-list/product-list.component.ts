af4443e18220a0fb170dd222addab46f44446d50
Edin Jašarević <edin.jasarevic@infinitymesh.com>
2020-03-07 17:44:08 +0100

    bindin with interpolation

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'pm-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: "pm-product-list",
+    templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+    pageTitle: string = "Product List";
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
