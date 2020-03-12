3b36080be40e201ca85085b2df2dbd16ee03d8d2
Edin Jašarević <edin.jasarevic@infinitymesh.com>
2020-03-12 21:34:35 +0100

    excepting handling

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,8 @@
 import { IProduct } from './product';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -12,6 +13,20 @@ export class ProductService {
     constructor(private http: HttpClient) {}
 
     getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl);
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data => console.log("All " + JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            errorMessage = `An error occured: ${err.error.message}`;
+        } else {
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.log(errorMessage);
+        return throwError(errorMessage);
     }
 }
