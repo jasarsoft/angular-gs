338f1592d1915740e00b4762a52ad5225874017c
Edin Jašarević <edin.jasarevic@infinitymesh.com>
2020-03-12 11:17:23 +0100

    passing data to a nested component

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
     styleUrls: ['./star.component.css']
 })
 export class StarComponent {
-    rating: number = 4;
+    @Input() rating: number;
     startWith: number;
 
     ngOnChanges(): void {
